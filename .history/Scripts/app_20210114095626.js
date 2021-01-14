19f2f19d1bed175357c32174ca4000758b389681
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-01-14 09:58:41 -0500

    fixed click me button

---
+++ .history/Scripts/app_20210114095626.js
@@ -0,0 +1,36 @@
+/*
+ * Custom Scripts go here!
+ */
+
+//IIFE - Immediately Invoked Function Expression
+//AKA - Anonymous Self-Executing Function
+(function()
+{
+    //Initialization Function
+    function Start()
+    {
+       
+       console.log("App Started...");
+
+       for (let index = 0; index < 5; index++) 
+       {
+           
+        console.log(index);
+          
+        let clickMeButton = document.getElementById("clickMeButton");
+
+        clickMeButton.addEventListener("click", ()=> 
+        {
+            console.log("Click me button was depressed :(");
+        });
+       }
+    }
+    //Event Listener
+    window.addEventListener("load", Start);
+
+})();
+
+
+
+
+ 
